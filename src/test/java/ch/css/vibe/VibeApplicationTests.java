7c668d0c2b7baf226de43ce8de7b173d5c945e9a
Vera Corten <vera.corten@hotmail.com>
2026-01-21 09:44:32 +0100

    initial

---
+++ src/test/java/ch/css/vibe/VibeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.css.vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
